ff379921aafa3169e738c7d4c44f9c7b40145c26
juan_ <rapidclimate@gmail.com>
2020-10-06 19:48:37 -0500

    sasasas

--- app/src/main/java/com/unitec/apptextoavoz/MainActivity.kt
+++ app/src/main/java/com/unitec/apptextoavoz/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.unitec.apptextoavoz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -23,6 +27,24 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
         //Iniciamos ahora i la varible tts para que ya no este en null
         tts= TextToSpeech(this,this)
+
+
+        hablar.setOnClickListener {
+            val intent =Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            try{
+                startActivityForResult(intent,CODIGO_PETICION)
+            }catch (e:Exception){  }
+        }
+
+        //Programamos el clicqueo del boton para que inptete lo escrito
+        interpretar.setOnClickListener {
+            if(fraseEscrita.text.isEmpty()){
+             Toast.makeText(this,"Debes escribir algo para que lo hable",Toast.LENGTH_LONG).show()
+            }else{
+                //Este metodo ahorita lo vamos a implementar
+                hablarTexto(fraseEscrita.text.toString())
+            }
+        }
 
         //KEMOSION!!!! VAMOS A ESCUCHAR ESA VOCESITA DE ANDROID, DE BIENVENIDA
         Timer("Bienvenida", false).schedule(1000){
@@ -47,6 +69,35 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
                 Log.i("PESIMO","NOOOOOOOOO, NO FUNCIONO EL LENGUAJE, ALGO ANDA MAL")
             }
 
+        }
+    }
+
+    //Esta funcion es la que nos ayudaa interpretar lo que se escriba en el texto de la frase
+    fun hablarTexto(textoHablar:String){
+        tts!!.speak(textoHablar, TextToSpeech.QUEUE_FLUSH, null,"")
+    }
+
+    //Este metodo es opcional pero se los recomiendo para limpiar la memoria de esta app cuando la cierren
+    override fun onDestroy(){
+        super.onDestroy()
+        if(tts!=null){
+            //En el caso de las aplicaciones de espionaje estos dos renglones NUNCA SE APAGAN.
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            CODIGO_PETICION->{
+                if(resultCode== RESULT_OK && null!=data){
+                    val result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    //Finalmente le vamos a decir a nuestro texto estatico que aqui nos muestre lo
+                    //Lo que dijimos peor en txto
+                    textoIntepretado.setText(result!![0])
+                }
+            }
         }
     }
 }
